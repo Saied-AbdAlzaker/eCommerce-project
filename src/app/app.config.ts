12caf42c846bf8909475c0d8ba3467eb19a7ef18
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-11-13 20:07:03 +0200

    update angular json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
@@ -24,7 +24,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
+  provideRouter(routes,withHashLocation()),
   provideClientHydration(withEventReplay()),
   provideHttpClient(withFetch(), withInterceptors([headersInterceptor, errorsInterceptor, loadingInterceptor])),
   provideAnimations(),
@@ -42,10 +42,7 @@ export const appConfig: ApplicationConfig = {
   provideAnimationsAsync(),
   providePrimeNG({
     theme: {
-      preset: Aura,
-      //  options: {
-      //   darkModeSelector: '.my-app-dark'
-      // }
+      preset: Aura
     }
   })
   ]
